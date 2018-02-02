d8d56cdce32c5f34376972fc9fac865ac6cff6fe
Thomas McMahon <tmcmahon@nerdery.com>
2018-02-01 20:27:31 -0600

    Updating the JS to include closing the search when hitting escape.

--- tw-search-scripts.js
+++ tw-search-scripts.js
@@ -1,10 +1,20 @@
 jQuery( document ).on( 'click', '.js-twSearch', function( event ) {
-	jQuery( '.twSearchFormWrapper' ).animate( { right: 0, easing: 'easein' }, 250 );
-	jQuery( '.twSearchPopup' ).show();
-	jQuery( '.twSearchBox' ).focus();
-	jQuery( '.twSearchBg' ).click( function() {
-		jQuery( '.twSearchPopup' ).hide();
-		jQuery( '.twSearchFormWrapper' ).animate( { right: '-100%' }, 50 );
-	} );
-	event.preventDefault();
+    jQuery( '.twSearchFormWrapper' ).animate( { right: 0, easing: 'easein' }, 250 );
+    jQuery( '.twSearchPopup' ).show();
+    jQuery( '.twSearchBox' ).focus();
+    jQuery( '.twSearchBg' ).click( function() {
+        closeSearch();
+    } );
+    jQuery( document ).on( 'keydown', function ( event ) {
+        if ( event.keyCode === 27 && jQuery('.twSearchBg').is(':visible')) { // 27 = Escape
+            closeSearch();
+        }
+    });
+
+    event.preventDefault();
 } );
+
+function closeSearch() {
+    jQuery( '.twSearchPopup' ).hide();
+    jQuery( '.twSearchFormWrapper' ).animate( { right: '-100%' }, 50 );
+}
